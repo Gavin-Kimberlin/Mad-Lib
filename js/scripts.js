c96175f74a0aac1ebd3b7593454ba3cad40e9c35
Erik Myers and Gavin Kimberlin <gavin@>
2019-06-05 09:52:53 -0700

    This adds javascript functions to a madlib game

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var person1Input = ("input#person1");
+    $(".noun1").append("blah blah");
+    $(".noun2").append("blah blah");
+    $(".fantastic-animal").append("blah blah");
+    $(".exclamation").append("blah blah");
+    $(".verbEnding").append("blah blah");
+    $(".adjective:").append("blah blah")
+    $(".nounPlural")append("blah blah")
+    $(".verb").append("blah blah");
+    $(".noun").append("blah blah");
+    $("#story").show();
+    event.preventDefault();
+  });
+});
