392a58cbafc63ec64e1723a802e1ecff4f2db092
Gavin Kimberlin and Lake Bramlett <gavin@>
2019-06-10 09:58:40 -0700

    refactor dry code

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,12 @@
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    var person1Input = ("input#person1");
-    $(".noun1").append("blah blah");
-    $(".noun2").append("blah blah");
-    $(".fantastic-animal").append("blah blah");
-    $(".exclamation").append("blah blah");
-    $(".verbEnding").append("blah blah");
-    $(".adjective:").append("blah blah")
-    $(".nounPlural")append("blah blah")
-    $(".verb").append("blah blah");
-    $(".noun").append("blah blah");
-    $("#story").show();
+$(document).ready(function () {
+  $('#formOne').submit(function (event) {
+    var blanks = ['noun1', 'noun2', 'fantastic-animal', 'lewd-exclamation', 'verbEnding', 'adjective', 'nounPlural', 'verb', 'noun'];
+
+    blanks.forEach(function(blank) {
+      var userInput = $('input#' + blank).val();
+      $('.' + blank).text(userInput);
+    });
+    $('#story').show();
     event.preventDefault();
   });
 });
